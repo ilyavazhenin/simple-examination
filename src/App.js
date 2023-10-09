6c8fb5d638f7aed7c399b047f4a31dd9efacd0bd
Ilia Vazhenin <heyheyftw@gmail.com>
2023-10-09 18:16:39 +0400

    init after cra

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      Here's gonna be a form with Q&A!
+      </header>
+    </div>
+  );
+}
+
+export default App;
